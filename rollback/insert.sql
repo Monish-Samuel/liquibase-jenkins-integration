c9983919131203123fada2c4b627a34d9c8c224c
monish <samuelmonish1@gmail.com>
2022-10-01 19:54:47 +0530

    new changes added

--- rollback/insert.sql
+++ rollback/insert.sql
@@ -1,1 +1,4 @@
-DELETE FROM jenkins."user" WHERE id=90
+DELETE FROM jenkins."user" WHERE id=91
+DELETE FROM jenkins."user" WHERE id=93
+DELETE FROM jenkins."user" WHERE id=94
+DELETE FROM jenkins."user" WHERE id=95
--- update/insert.sql
+++ update/insert.sql
@@ -1,6 +1,6 @@
 INSERT INTO jenkins."user" (id,first_name,last_name,role)
-SELECT 90,'Monish','Samuel','Manager'
+SELECT 91,'Monish','Samuel','Manager'
 WHERE
     NOT EXISTS (
-        SELECT id FROM jenkins."user" WHERE id = 90
+        SELECT id FROM jenkins."user" WHERE id = 91
     );
