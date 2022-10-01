d5e46bb36382ffdfef54b34869aeda8d142ed8b8
Monish Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2022-10-01 19:29:49 +0530

    Update insert.sql

--- update/insert.sql
+++ update/insert.sql
@@ -1,34 +1,6 @@
 INSERT INTO jenkins."user" (id,first_name,last_name,role)
-SELECT 2,'Monish','Samuel','member'
+SELECT 90,'Monish','Samuel','Manager'
 WHERE
     NOT EXISTS (
-        SELECT id FROM jenkins."user" WHERE id = 2
-    );
-
-INSERT INTO jenkins."user" (id,first_name,last_name,role)
-SELECT 3,'Monish','Samuel','fresher'
-WHERE
-    NOT EXISTS (
-        SELECT id FROM jenkins."user" WHERE id = 3
-    );
- 
-INSERT INTO jenkins."user" (id,first_name,last_name,role)
-SELECT 4,'Monish','Samuel','admin'
-WHERE
-    NOT EXISTS (
-        SELECT id FROM jenkins."user" WHERE id = 4
-    );
-    
-INSERT INTO jenkins."user" (id,first_name,last_name,role)
-SELECT 5,'Monish','Samuel','Manager'
-WHERE
-    NOT EXISTS (
-        SELECT id FROM jenkins."user" WHERE id = 5
-    );
-
-INSERT INTO jenkins."user" (id,first_name,last_name,role)
-SELECT 99,'Monish','Samuel','Manager'
-WHERE
-    NOT EXISTS (
-        SELECT id FROM jenkins."user" WHERE id = 00
+        SELECT id FROM jenkins."user" WHERE id = 90
     );
