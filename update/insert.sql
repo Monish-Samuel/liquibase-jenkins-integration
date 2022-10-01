1782d208744fc52ab858d067367dfc52c078761a
Monish Samuel <79988944+Monish-Samuel@users.noreply.github.com>
2022-10-01 17:48:17 +0530

    Create insert.sql

---
+++ update/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO jenkins."user" (id,first_name,last_name,role)
+VALUES (2,'Monish','Samuel','member');
+
+INSERT INTO jenkins."user" (id,first_name,last_name,role)
+VALUES (3,'Monish','Samuel','member');
